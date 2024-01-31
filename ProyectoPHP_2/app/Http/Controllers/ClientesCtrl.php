27da4369931a7488abc4af8e3c5f27781af5f6e2
juanmaguerrerx <jguevid1609@g.educaand.es>
2024-01-31 17:42:57 +0100

    making_skell

--- ProyectoPHP_2/app/Http/Controllers/ClientesCtrl.php
+++ ProyectoPHP_2/app/Http/Controllers/ClientesCtrl.php
@@ -2,9 +2,64 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Clientes;
 use Illuminate\Http\Request;
 
 class ClientesCtrl extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Clientes $clientes)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Clientes $clientes)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Clientes $clientes)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Clientes $clientes)
+    {
+        //
+    }
 }
--- ProyectoPHP_2/app/Http/Controllers/IncidenciasCtrl.php
+++ ProyectoPHP_2/app/Http/Controllers/IncidenciasCtrl.php
@@ -2,9 +2,64 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Incidencias;
 use Illuminate\Http\Request;
 
 class IncidenciasCtrl extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Incidencias $incidencias)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Incidencias $incidencias)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Incidencias $incidencias)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Incidencias $incidencias)
+    {
+        //
+    }
 }
