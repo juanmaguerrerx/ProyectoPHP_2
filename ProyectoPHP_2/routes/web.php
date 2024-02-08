4e46f7a29abb97b3fced5219169b45037bdd9b38
juanmaguerrerx <jguevid1609@g.educaand.es>
2024-02-08 18:46:18 +0100

    optimizar css tabla

--- ProyectoPHP_2/routes/web.php
+++ ProyectoPHP_2/routes/web.php
@@ -19,7 +19,6 @@ use App\Http\Controllers\TareasCtrl;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
 Route::get('/', function () {
     return view('dashboard');
 });
@@ -33,51 +32,22 @@ Route::get('/dashboard', [DashboardCtrl::class, 'mostrarDash'])->name('dashboard
 /**
  * Rutas Clientes
  */
-// GET
 Route::resource('clientes', ClientesCtrl::class);
-// Route::get('/clientes', [ClientesCtrl::class, 'index'])->name('clientes.index');
-// Route::get('/clientes-create', [ClientesCtrl::class, 'create'])->name('clientes.create');
-// Route::get('/clientes-edit', [ClientesCtrl::class, 'edit'])->name('clientes.edit');
-// // POST
-// Route::post('/clientes-create', [ClientesCtrl::class, 'store'])->name('clientes.store');
-
-
 
 
 /**
  * Rutas Operarios
  */
-// GET
-
 Route::resource('empleados', EmpleadosCtrl::class);
-// Route::get('/empleados', [EmpleadosCtrl::class, 'index'])->name('empleados.index');
-// Route::get('/empleados-create', [EmpleadosCtrl::class, 'create'])->name('empleados.create');
-// Route::get('/empleados-edit', [EmpleadosCtrl::class, 'edit'])->name('empleados.edit');
-// // POST
-// Route::post('/empleados-create', [EmpleadosCtrl::class, 'store'])->name('empleados.store');
 
 
 /**
  * Rutas Incidencias
  */
-// GET
 Route::resource('incidencias', IncidenciasCtrl::class);
-// Route::get('/incidencias', [IncidenciasCtrl::class, 'index'])->name('incidencias.index');
-// Route::get('/incidencias-create', [IncidenciasCtrl::class, 'create'])->name('incidencias.create');
-// Route::get('/incidencias-show', [IncidenciasCtrl::class, 'show'])->name('incidencias.show');
-// Route::get('/incidencias-edit', [IncidenciasCtrl::class, 'edit'])->name('incidencias.edit');
-// POST
+
 
 /**
  * Rutas Cuotas
  */
-// GET
-
 Route::resource('cuotas', CuotasCtrl::class);
-// Route::get('/cuotas', [CuotasCtrl::class, 'index'])->name('cuotas.index');
-// Route::get('/cuotas-create', [CuotasCtrl::class, 'create'])->name('cuotas.create');
-// Route::get('/cuotas/{id?}', [CuotasCtrl::class, 'edit'])->name('cuotas.edit');
-
-
-// POST
-// Route::post('/cuotas-create', [CuotasCtrl::class, 'store'])->name('cuotas.store');
